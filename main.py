b7040bfb09df7dfb3e7972121cd0e5fd92795e77
daniel-pawelko-ics20 <82897918+daniel-pawelko-ics20@users.noreply.github.com>
2022-01-24 11:55:49 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Created by: Daniel Pawelko
+# Created on: Jan 2022
+# Max Num
+
+from random import randint
+
+
+def main():
+    # main function for max number
+
+    # create array with 10 random numbers/process
+    arr = [randint(1, 100) for rand in range(10)]
+
+    # define biggest variable
+    biggest = arr = 0
+
+    # get biggest/process
+    for num in arr:
+        print(f"The random number {num + 1} is: {num}")
+        if num > biggest:
+            biggest = num
+
+    # output
+    print(f"\nBiggest number is {biggest}")
+
+    # done
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
